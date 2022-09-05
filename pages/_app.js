90fd10fa08ca04f6947202d6657a95065ba1657f
Waleed <waleed@fox.so>
2022-09-06 03:57:34 +0500

    a

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
-import '../styles/globals.css'
-
+import "../styles/globals.css"
+console.log(process.env.sample_api)
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return <Component {...pageProps} />
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head"
 import Image from "next/image"
 import styles from "../styles/Home.module.css"
-console.log(process.env)
+console.log(process.env.sample_api)
 export default function Home() {
 	return (
 		<div className={styles.container}>
